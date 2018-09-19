a2e8986aea6af6c6189b8405b2716e45b782ac34
anmolschauhan <31269280+anmolschauhan@users.noreply.github.com>
2018-09-19 21:13:35 +0530

    Create persistent_binary_tries.cpp

---
+++ persistent_binary_tries.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+#define MAXN 500050
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+struct node
+{
+    int val;
+    node *child[2];
+    node()
+    {
+        val = 0;
+        for(int i = 0; i < 2; i++)
+            child[i] = NULL;
+    }
+};
+
+node *version[MAXN];
+
+void initialize()
+{
+    version[0] = new node();
+    version[0]->child[0] = version[0];
+    version[0]->child[1] = version[0];
+}
+
+node* insert(node *prev, unsigned val, int bitcount)
+{
+    node *root = new node();
+    node *temp = root;
+    for(int i = bitcount; i >= 0; i--)
+    {
+        root->val = prev->val + 1;
+        int loc = (val >> i) & 1;
+        int opposite = 1 - loc;
+        root->child[opposite] = prev->child[opposite];
+        root->child[loc] = new node();
+        root = root->child[loc];
+        prev = prev->child[loc];
+    }
+    root->val = prev->val + 1;
+    return temp;
+}
+
+int maxXOR(node *curr, node *prev, int x, int bitcount)
+{
+    int result = 0;
+    for(int i = bitcount; i >= 0; i--)
+    {
+        int loc = (x >> i) & 1;
+        int opposite = 1 - loc;
+        int count = (curr->child[opposite]->val) - (prev->child[opposite]->val);
+        if(count == 0)
+        {
+            curr = curr->child[loc];
+            prev = prev->child[loc];
+        }
+        else
+        {
+            curr = curr->child[opposite];
+            prev = prev->child[opposite];
+            result |= (1 << i);
+        }
+    }
+    return result ^ x;
+}
+
+int kthSmallest(node *curr, node *prev, int k, int bitcount)
+{
+    int result = 0;
+    for(int i = bitcount; i >= 0; i--)
+    {
+        int count = curr->child[0]->val - prev->child[0]->val;
+        if(k > count)
+        {
+            curr = curr->child[1];
+            prev = prev->child[1];
+            result |= (1 << i);
+            k -= count;
+        }
+        else
+        {
+            curr = curr->child[0];
+            prev = prev->child[0];
+        }
+    }
+    return result;
+}
+
+int smallerThanX(node *curr, node *prev, int x, int bitcount)
+{
+    int result = 0;
+    for(int i = bitcount; i >= 0; i--)
+    {
+        int loc = (x >> i) & 1;
+        int opposite = 1 - loc;
+        if(loc == 1)
+        {
+            int left = curr->child[0]->val - prev->child[0]->val;
+            result += left;
+            curr = curr->child[1];
+            prev = prev->child[1];
+        }
+        else
+        {
+            curr = curr->child[0];
+            prev = prev->child[0];
+        }
+    }
+    int temp = curr->val - prev->val;
+    return result + temp;
+}
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    initialize();
+    int n, last = 1;
+    cin >> n;
+    while(n--)
+    {
+        int x;
+        cin >> x;
+        if(x == 0)
+        {
+            int k;
+            cin >> k;
+            version[last] = insert(version[last-1], k, 20);
+            last++;
+        }
+        else if(x == 1)
+        {
+            int l, r, k;
+            cin >> l >> r >> k;
+            cout << maxXOR(version[r], version[l-1], k, 20) << endl;
+        }
+        else if(x == 2)
+        {
+            int k;
+            cin >> k;
+            last -= k;
+        }
+        else if(x == 3)
+        {
+            int l, r, k;
+            cin >> l >> r >> k;
+            cout << smallerThanX(version[r], version[l-1], k, 20) << endl;
+        }
+        else if(x == 4)
+        {
+            int l, r, k;
+            cin >> l >> r >> k;
+            cout << kthSmallest(version[r], version[l-1], k, 20) << endl;
+        }
+    }
+	return 0;
+}
